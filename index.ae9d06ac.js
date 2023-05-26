3e4946fce0a63ab79da961476986a8c728a78650
DiakovSasha <102146786+DiakovSasha@users.noreply.github.com>
2023-05-26 01:25:52 +0000

    Deploying to gh-pages from @ DiakovSasha/project-ice-cream@63f55e10cc7e3e9e3d2858983d43f56a62984272 🚀

---
+++ index.ae9d06ac.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:30,loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});console.log("hello");
+//# sourceMappingURL=index.ae9d06ac.js.map
---
+++ index.d3cf41ff.js
@@ -0,0 +1,2 @@
+!function(){new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:30,loop:!0,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});console.log("hello")}();
+//# sourceMappingURL=index.d3cf41ff.js.map
